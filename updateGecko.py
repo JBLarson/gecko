b4ba16c3c8aa94714cc485b5a88d3a91f803b792
JB Larson <68449234+JBLarson@users.noreply.github.com>
2021-09-13 22:08:05 -0400

    Update updateGecko.py

--- updateGecko.py
+++ updateGecko.py
@@ -1,97 +1,178 @@
 
-import datetime
+from datetime import datetime
+from time import strftime
+import dateparser
 import json
-from geckoFuncz import unixToDatetime, datetimeToUnix
+from geckoFuncz import unixToDatetime, datetimeToUnix, analyzeTokenFunc, stdDevFunc, upToDateFunc
 import pycoingecko
 
-# import coinGecko data
-jsonInAddr = 'data/gTest.json'
 
-with open(jsonInAddr, 'r') as f:
-	geckoData = json.load(f)
+try:
+	jsonInAddr = 'data/geckoUpdate' + '.json'
+	with open(jsonInAddr, 'r') as f:
+		geckoData = json.load(f)
+
+except:
+	# import coinGecko data
+	jsonInAddr = 'data/geckoAnalysis.json'
+
+	with open(jsonInAddr, 'r') as f:
+		geckoData = json.load(f)
 
 geckoKeys = list(geckoData.keys())
 
-geckoDataBtcUsd = geckoData['BtcUsd']
+
+
+
+upToDate = upToDateFunc(geckoData)
+
+
+if upToDate == False:
+
+	time = datetime.now()
+	dtRn = str(strftime("%x") + " " + strftime("%X"))
+	justTime, justDate = strftime("%X"), strftime("%x")
+	print("\nStarted Update on: " + str(justDate) + " at: " + str(justTime) + "\n")
+
+
+
+
+	# datetime / epoch variables
+	today = datetime.now()
+	today = str(today).split(".")
+	today = today[0]
+	todaySplit = str(today).split(" ")
+	todayYmd = todaySplit[0]
+	unixToday = datetimeToUnix(today)
+
+
+
+	def getCoinDict(coin, baseCurrency, fromTimeStamp, toTimestamp):
+		from pycoingecko import CoinGeckoAPI
+		cg = CoinGeckoAPI()
+
+		coinApiRez = cg.get_coin_market_chart_range_by_id(id=coin, vs_currency=baseCurrency, from_timestamp=fromTimeStamp, to_timestamp=toTimestamp) # coin gecko coinApiRez
+		coinRezPrices = coinApiRez['prices']
+		coinRezVolumes = coinApiRez['total_volumes']
+
+		volumeDict, priceDict = {}, {}
+		for price in coinRezPrices:
+			priceIndex = coinRezPrices.index(price)
+			unixTime = price[0]
+			volume = coinRezVolumes[priceIndex][1]
+			unixTime = int(str(unixTime)[:-3])
+			price = price[1]
+			localDT = unixToDatetime(unixTime)
+
+			priceDict.update({localDT: price})
+			volumeDict.update({localDT: volume})
+
+		returnDict = {"base": baseCurrency, "quote": coin, "data": priceDict, "volumeData": volumeDict}
+
+		return returnDict
+
+
+
+
+	def updateGeckoData(currentGeckoDict):
+		currentBase = currentGeckoDict['base']
+		currentQuote = currentGeckoDict['quote']
+		currentPair = currentQuote + "/" + currentBase
+		currentGeckoPriceData = currentGeckoDict['data']
+		currentGeckoVolumeData = currentGeckoDict['volumeData']
+
+		currentDates = list(currentGeckoPriceData.keys())
+		currentDates.sort(reverse=False)
+		lastDate = currentDates[-1]
+		if lastDate < todayYmd:
+			unixLastDate = datetimeToUnix(lastDate)
+			updateData = getCoinDict(currentQuote, currentBase, unixLastDate, unixToday)
+			updatedPriceData = updateData['data']
+			updatedVolumeData = updateData['volumeData']
+
+			currentGeckoPriceData.update(updatedPriceData)
+			currentGeckoVolumeData.update(updatedVolumeData)
+			updateFuncOutput = currentPair + " has been updated"
+
+		else:
+			updateFuncOutput = currentPair + " is up to date"
+
+		return updateFuncOutput
+
+
+	for geckoKey in geckoKeys:
+		currentGeckoData = geckoData[geckoKey]
+		updateData = updateGeckoData(currentGeckoData)
+		print(updateData)
+
+
+	time = datetime.now()
+	dtRn = str(strftime("%x") + " " + strftime("%X"))
+	justTime, justDate = strftime("%X"), strftime("%x")
+	print("\nCompleted Update on: " + str(justDate) + " at: " + str(justTime) + "\n")
+
+
+	time = datetime.now()
+	dtRn = str(strftime("%x") + " " + strftime("%X"))
+	justTime, justDate = strftime("%X"), strftime("%x")
+	print("\nStarted sampleStats on: " + str(justDate) + " at: " + str(justTime) + "\n")
 
 
 
 
 
-# datetime / epoch variables
-today = datetime.datetime.now()
-today = str(today).split(".")
-today = today[0]
-todaySplit = str(today).split(" ")
-todayYmd = todaySplit[0]
-unixToday = datetimeToUnix(today)
 
+	# function to run analysis function on all pairs in gecko dictionary
+	def analyzeAllTokens(geckoData):
+		analysisList = []
 
+		quoteKeys = list(geckoData.keys())
 
-def getCoinDict(coin, baseCurrency, fromTimeStamp, toTimestamp):
-	from pycoingecko import CoinGeckoAPI
-	cg = CoinGeckoAPI()
+		for quoteKey in quoteKeys:
+			pairDict = geckoData[quoteKey]
+			analyzePair = analyzeTokenFunc(pairDict)
+			analysisList.append(analyzePair)
+		return analysisList
 
-	coinApiRez = cg.get_coin_market_chart_range_by_id(id=coin, vs_currency=baseCurrency, from_timestamp=fromTimeStamp, to_timestamp=toTimestamp) # coin gecko coinApiRez
-	coinRezPrices = coinApiRez['prices']
-	coinRezVolumes = coinApiRez['total_volumes']
+	analyzeAll = analyzeAllTokens(geckoData)
 
-	volumeDict, priceDict = {}, {}
-	for price in coinRezPrices:
-		priceIndex = coinRezPrices.index(price)
-		unixTime = price[0]
-		volume = coinRezVolumes[priceIndex][1]
-		unixTime = int(str(unixTime)[:-3])
-		price = price[1]
-		localDT = unixToDatetime(unixTime)
-
-		priceDict.update({localDT: price})
-		volumeDict.update({localDT: volume})
-
-	returnDict = {"base": baseCurrency, "quote": coin, "data": priceDict, "volumeData": volumeDict}
-
-	return returnDict
+	for aDict in analyzeAll:
+		aDictPair = aDict['pair']
+		
+		baseKeys = geckoData.keys()
+		for baseKey in baseKeys:
+			currentGeckoDict = geckoData[baseKey]
+			currentGekkoQuote = currentGeckoDict['quote']
+			currentGekkoBase = currentGeckoDict['base']
+			currentGekkoPair = str(currentGekkoQuote).capitalize() + str(currentGekkoBase).capitalize()
+			if aDictPair == currentGekkoPair:
+				currentGeckoDict.update(aDict)
 
 
 
 
-def updateGeckoData(currentGeckoDict):
-	currentBase = currentGeckoDict['base']
-	currentQuote = currentGeckoDict['quote']
-	currentGeckoPriceData = currentGeckoDict['data']
-	currentGeckoVolumeData = currentGeckoDict['volumeData']
-
-	currentDates = list(currentGeckoPriceData.keys())
-	currentDates.sort(reverse=False)
-	lastDate = currentDates[-1]
-	if lastDate < todayYmd:
-		unixLastDate = datetimeToUnix(lastDate)
-		updateData = getCoinDict(currentQuote, currentBase, unixLastDate, unixToday)
-		updatedPriceData = updateData['data']
-		updatedVolumeData = updateData['volumeData']
-
-		currentGeckoPriceData.update(updatedPriceData)
-		currentGeckoVolumeData.update(updatedVolumeData)
-
-	else:
-		print("Current dictionary is up to date")
+	for geckoKey in geckoKeys:
+		currentGdict = geckoData[geckoKey]
+		stdDev = stdDevFunc(currentGdict)
+		currentGdict.update({'stdDev': stdDev})
 
 
 
-updateTest = updateGeckoData(geckoDataBtcUsd)
+	time = datetime.now()
+	dtRn = str(strftime("%x") + " " + strftime("%X"))
+	justTime, justDate = strftime("%X"), strftime("%x")
+	print("\nCompleted sample stats on: " + str(justDate) + " at: " + str(justTime) + "\n")
 
 
 
+	jsonOutAddr = 'data/geckoUpdate' + '.json'
+	try:
+		with open(jsonOutAddr, 'w') as fp1: json.dump(geckoData, fp1)
+		print("\nSuccess Creating Crypto Json on/at: " + str(jsonOutAddr) + "\n")
+
+	except Exception as e:
+		print(e)
 
 
-jsonOutAddr = 'data/gTest' + '.json'
-try:
-	with open(jsonOutAddr, 'w') as fp1: json.dump(geckoData, fp1)
-
-
-	print("\nSuccess Creating Crypto Json on/at: " + str(jsonOutAddr) + "\n")
-
-except Exception as e:
-	print(e)
-
-
+elif upToDate == True:
+	print("\nPrice Data is current\n")
