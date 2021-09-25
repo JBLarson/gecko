756f74ba5ac6719880297e3279559ec44f98b27f
JB Larson <68449234+JBLarson@users.noreply.github.com>
2021-09-24 22:38:51 -0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-/usr/bin/python3 fetchGecko.py && echo && echo 'Success fetching Coin Gecko Data' && /usr/bin/python3 sampleStats.py && echo && echo 'Success creating sample statistics' && /usr/bin/python3 sma.py && echo && echo 'Success creating Simple Moving Averages' && echo && echo 'Launching Plot.py GUI' && /usr/bin/python3 plot.py &&
+/usr/bin/python3 fetchGecko.py && echo && echo 'Success fetching Coin Gecko Data and creating sample stats' && /usr/bin/python3 sma.py && echo && echo 'Success creating Simple Moving Averages' && echo && echo 'Launching Plot.py GUI' && /usr/bin/python3 plot.py &&
