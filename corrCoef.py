1743bb8043dfcf89de62bc0c522c5cf944e8ae0b
JB Larson <68449234+JBLarson@users.noreply.github.com>
2021-09-28 07:20:03 -0400

    Update corrCoef.py

--- corrCoef.py
+++ corrCoef.py
@@ -19,42 +19,62 @@ geckoData = readJsonFunc('data/geckoAnalysis2.json')
 geckoKeys = list(geckoData.keys())
 
 
+
+
 def correlationCoefficient(list1, list2):
 	corrCoef = np.corrcoef(list1, list2)
 	return corrCoef
 
 
 
-def btcCorrCoefFunc(tokenPair):
+def corrCoefFunc(tokenPair):
 	tokenPrices = list(geckoData[tokenPair]['data'].values())
 	if 'Usd' in tokenPair:	btcPrices = list(geckoData['BtcUsd']['data'].values())
-	if 'Eur' in tokenPair:	btcPrices = list(geckoData['BtcEur']['data'].values())
-	corrCoefOutput = float(correlationCoefficient(btcPrices, tokenPrices)[0,1])
+	elif 'Eur' in tokenPair:	btcPrices = list(geckoData['BtcEur']['data'].values())
+	corrCoefBtc = ro6(float(correlationCoefficient(btcPrices, tokenPrices)[0,1]))
+
+	if 'Usd' in tokenPair:	ethPrices = list(geckoData['EthUsd']['data'].values())
+	elif 'Eur' in tokenPair:	ethPrices = list(geckoData['EthEur']['data'].values())
+	corrCoefEth = ro6(float(correlationCoefficient(ethPrices, tokenPrices)[0,1]))
+
+	corrCoefOutput = {'btc': corrCoefBtc, 'eth': corrCoefEth}
 
 	return corrCoefOutput
 
 
-#for geckoKey in geckoKeys:
-#	btcCorrCoef = btcCorrCoefFunc(geckoKey)
-#	print(geckoKey + " BTC Corr Coef: " + str(btcCorrCoef))
+def allCorrCoefFunc(geckoKey):
+
+	tokenPrices = list(geckoData[geckoKey]['data'].values())
+
+	if 'Usd' in geckoKey:
+		btcPrices = list(geckoData['BtcUsd']['data'].values())
+		ethPrices = list(geckoData['EthUsd']['data'].values())
+		adaPrices = list(geckoData['AdaUsd']['data'].values())
+		linkPrices = list(geckoData['LinkUsd']['data'].values())
+		daiPrices = list(geckoData['DaiUsd']['data'].values())
 
 
+	elif 'Eur' in geckoKey:
+		btcPrices = list(geckoData['BtcEur']['data'].values())
+		ethPrices = list(geckoData['EthEur']['data'].values())
+		adaPrices = list(geckoData['AdaEur']['data'].values())
+		linkPrices = list(geckoData['LinkEur']['data'].values())
+		daiPrices = list(geckoData['DaiEur']['data'].values())
 
 
+	
+
+	corrCoefBtc = ro6(float(correlationCoefficient(btcPrices, tokenPrices)[0,1]))
+	corrCoefEth = ro6(float(correlationCoefficient(ethPrices, tokenPrices)[0,1]))
+	corrCoefAda = ro6(float(correlationCoefficient(adaPrices, tokenPrices)[0,1]))
+	corrCoefLink = ro6(float(correlationCoefficient(linkPrices, tokenPrices)[0,1]))
+	corrCoefDai = ro6(float(correlationCoefficient(daiPrices, tokenPrices)[0,1]))
 
 
+	corrCoefOutput = {'btc': corrCoefBtc, 'eth': corrCoefEth, 'ada': corrCoefAda, 'link': corrCoefLink, 'dai': corrCoefDai}
 
-def corrCoefNFunc(tokenPair):
-	tokenPrices = list(geckoData[tokenPair]['data'].values())
-	if 'Usd' in tokenPair:	btcPrices = list(geckoData['BtcUsd']['data'].values())
-	elif 'Eur' in tokenPair:	btcPrices = list(geckoData['BtcEur']['data'].values())
-	corrCoefBtc = float(correlationCoefficient(btcPrices, tokenPrices)[0,1])
-
-	if 'Usd' in tokenPair:	ethPrices = list(geckoData['EthUsd']['data'].values())
-	elif 'Eur' in tokenPair:	ethPrices = list(geckoData['EthEur']['data'].values())
-	corrCoefEth = float(correlationCoefficient(ethPrices, tokenPrices)[0,1])
-
-	corrCoefOutput = {'btc': corrCoefBtc, 'eth': corrCoefEth}
+	#except Exception as e:
+	#	print("\nException: " + str(e) + "\n")
 
 	return corrCoefOutput
 
@@ -63,8 +83,9 @@ def corrCoefNFunc(tokenPair):
 
 
 for geckoKey in geckoKeys:
-	corrCoef = corrCoefNFunc(geckoKey)
+	corrCoef = allCorrCoefFunc(geckoKey)
 	print("Currency Pair: " + geckoKey + " Corr Coefs: " + str(corrCoef))
+
 
 
 
