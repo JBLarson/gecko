f5789b854593580fe411833edcb8c7017817457f
JB Larson <68449234+JBLarson@users.noreply.github.com>
2021-09-30 21:57:18 -0400

    Update analyzeCC.py

--- analyzeCC.py
+++ analyzeCC.py
@@ -3,7 +3,15 @@
 from geckoFuncz import *
 
 
+
+echoDtOutput = echoDt('Started', "Correlation Coefficient Analysis")
+
+
+
 ccData = readJsonFunc('data/cc.json')
+
+
+
 
 
 ccTimeFrames = list(ccData.keys())
@@ -13,15 +21,37 @@ ccTimeFrames = list(ccData.keys())
 geckoKeys = list(ccData[ccTimeFrames[0]].keys())
 
 
-for ccTimeFrame in ccTimeFrames[-1:]:
+def ccStats(currentCCdata):
+	currentCCList = []
+	firstCC = currentCCdata[geckoKeys[0]]
+
+	minCorr, maxCorr, corrSum = {geckoKeys[0]: firstCC}, {geckoKeys[0]: firstCC}, 0
+
+	for currentCompKey in geckoKeys:
+		currentCC = currentCCdata[currentCompKey]
+
+		currentCCList.append(currentCC)
+		#print(str(currentCompKey) + "  " + str(currentCC))
+		corrSum += currentCC
+	#print(currentCCList)
+
+	avgCorr = corrSum / len(geckoKeys)
+
+	ccStatOutput = {'max': maxCorr, 'min': minCorr, 'avg': avgCorr}
+
+	return ccStatOutput
+
+
+for ccTimeFrame in ccTimeFrames:
 	print("\n\n" + str(ccTimeFrame) + "-day moving avg\n")
 	currentCCtimeframe = ccData[ccTimeFrame]
 	for geckoKey in geckoKeys:
-		print("\nCorrelation Coefficient data for: " + str(geckoKey))
 		currentCCdata = currentCCtimeframe[geckoKey]
 		#print(currentCCdata)
-		for currentCompKey in geckoKeys:
+		currentCCStats = ccStats(currentCCdata)
 
-			currentCC = currentCCdata[currentCompKey]
-			print(str(currentCompKey) + "  " + str(currentCC))
+		print("\nCC stats for: " + str(geckoKey) + " " + str(currentCCStats))
 
+
+
+echoDtOutput = echoDt('Completed', "Correlation Coefficient Analysis")
