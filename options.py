ee66340f1565d348d7b66f44d7e15725edec9373
JB Larson <68449234+JBLarson@users.noreply.github.com>
2021-12-11 01:52:46 -0500

    Update options.py

--- options.py
+++ options.py
@@ -11,75 +11,59 @@ echoDtOutput = echoDt('Started', "Option Analysis")
 ro1, ro2, ro4, ro6, ro8 = lambda x : round(x, 1), lambda x : round(x, 2), lambda x : round(x, 4), lambda x : round(x, 6), lambda x : round(x, 8)
 
 
-def putReturnFunc(currency, strikePrice, expPrice, numContracts, contractCost):
 
+def putReturnFunc(currency, strikePrice, expPrice, numContracts, contractCost):
 	if currency == 'btc':
 		qtyBtc = numContracts * 0.01
 		cost = ro2(numContracts * 0.01 * contractCost)
 		fee = 0.15 * numContracts
-
 		if strikePrice > expPrice:
 			btcDifference = strikePrice - expPrice
-
 			preFeeValue = btcDifference * qtyBtc
 			value = ro2(preFeeValue - fee)
 
-		else:
-			value = ro2(-(cost + fee))
+		else:	value = ro2(-(cost + fee))
 
 	elif currency == 'eth':
-
 		qtyEth = numContracts * 0.1
 		cost = ro2(numContracts * 0.1 * contractCost)
 		fee = 0.15 * numContracts
-
 		if strikePrice > expPrice:
 			ethDifference = strikePrice - expPrice
-
-			preFeeValue = ethDifference * qtyBtc
+			preFeeValue = ethDifference * qtyEth
 			value = ro2(preFeeValue - fee)
 
-		else:
-			value = ro2(-(cost + fee))
-
-
+		else:	value = ro2(-(cost + fee))
 
 	putDictionary = {'strike': strikePrice, 'expiryPrice': expPrice, 'value': value, 'cost': cost, 'fee': fee, 'qty': numContracts}
-
 	return putDictionary
 
 
 
-def callReturnFunc(currency, strikePrice, expPrice, numContracts, contractCost):
 
+def callReturnFunc(currency, strikePrice, expPrice, numContracts, contractCost):
 	if currency == 'btc':
 		qtyBtc = numContracts * 0.01
 		cost = ro2(numContracts * 0.01 * contractCost)
 		fee = 0.15 * numContracts
-
 		if strikePrice < expPrice:
 			btcDifference = expPrice - strikePrice
-
 			preFeeValue = btcDifference * qtyBtc
 			value = ro2(preFeeValue - fee)
 
-		else:
-			value = ro2(-(cost + fee))
+		else:	value = ro2(-(cost + fee))
 
 	elif currency == 'eth':
 
 		qtyEth = numContracts * 0.1
 		cost = ro2(numContracts * 0.1 * contractCost)
 		fee = 0.15 * numContracts
-
 		if strikePrice < expPrice:
 			ethDifference = expPrice - strikePrice
-
-			preFeeValue = ethDifference * qtyBtc
+			preFeeValue = ethDifference * qtyEth
 			value = ro2(preFeeValue - fee)
 
-		else:
-			value = ro2(-(cost + fee))
+		else:	value = ro2(-(cost + fee))
 
 
 
@@ -100,52 +84,49 @@ def strangleFunc(callOption, putOption):
 
 	strangleDict = {'net': netValue, 'expiry': putOption['expiryPrice'], 'putStrike': putOption['strike'], 'callStrike': callOption['strike'], 'callCost': callCost, 'putCost': putCost}
 
-	#print(callOption)
-	#print(putOption)
-	#print("\nNet Option Value: $" + str(netValue))
 	return strangleDict
 
 
-#print("Function output")
-#testStrangle = strangleFunc(testCall, testPut)
-#print(testStrangle)
 
-def strangleResults(lowRange, highRange, rangeStep):
+def strangleEth(lowRange, highRange, rangeStep):
 	strangleResultList = []
 	for expiryPrice in np.arange(lowRange, highRange, rangeStep):
-		testCall = callReturnFunc('btc', 50000, expiryPrice, 1, 1255)
-		testPut = putReturnFunc('btc', 50000, expiryPrice, 1, 3607)
+		testCall = callReturnFunc('eth', 4000, expiryPrice, 1, 390)
+		testPut = putReturnFunc('eth', 4000, expiryPrice, 1, 400)
 		strangle = strangleFunc(testCall, testPut)
 		strangleResultList.append(strangle)
 	return strangleResultList
 
 
 
-strangleList = strangleResults(42000, 60000, 500)
-#print(strangleList)
+
+def strangleBtc(lowRange, highRange, rangeStep):
+	strangleResultList = []
+	for expiryPrice in np.arange(lowRange, highRange, rangeStep):
+		testCall = callReturnFunc('btc', 50000, expiryPrice, 1, 1200)
+		testPut = putReturnFunc('btc', 50000, expiryPrice, 2, 2900)
+		strangle = strangleFunc(testCall, testPut)
+		strangleResultList.append(strangle)
+	return strangleResultList
 
 
-for strangleResult in strangleList:
+
+
+
+#strangleEth = strangleEth(3000, 5000, 200)
+strangleBtc = strangleBtc(42000, 60001, 250)
+
+
+
+#for strangleResult in strangleEth:
+for strangleResult in strangleBtc:
 	strangleCost = ro2(strangleResult['putCost'] + strangleResult['callCost'])
-	#print(str(strangleResult['expiry']) + " " + str(strangleResult['net']))
 	print(strangleResult)
 
 
 
-"""
-testContractPrice = 2593
-testQty = 1
-testStrikePrice = 48000
 
-for btcPrice in np.arange(45000, 48100, 100):
-
-	currentOptionDict = putReturnFunc('btc', testStrikePrice, btcPrice, testQty, testContractPrice)
-
-	print(currentOptionDict)
-
-"""
 
 
 
 echoDtOutput = echoDt('Completed', "Option Analysis")
-
