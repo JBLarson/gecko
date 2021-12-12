08039752ebfb2cf7527e8df30a58ae56bf75bdd5
JB Larson <68449234+JBLarson@users.noreply.github.com>
2021-12-12 01:43:46 -0500

    Update options.py

--- options.py
+++ options.py
@@ -2,6 +2,8 @@
 
 import numpy as np
 from geckoFuncz import *
+import matplotlib.pyplot as plt
+
 
 echoDtOutput = echoDt('Started', "Option Analysis")
 
@@ -9,7 +11,6 @@ echoDtOutput = echoDt('Started', "Option Analysis")
 
 #lambda functions for rounding
 ro1, ro2, ro4, ro6, ro8 = lambda x : round(x, 1), lambda x : round(x, 2), lambda x : round(x, 4), lambda x : round(x, 6), lambda x : round(x, 8)
-
 
 
 def putReturnFunc(currency, strikePrice, expPrice, numContracts, contractCost):
@@ -37,7 +38,6 @@ def putReturnFunc(currency, strikePrice, expPrice, numContracts, contractCost):
 
 	putDictionary = {'strike': strikePrice, 'expiryPrice': expPrice, 'value': value, 'cost': cost, 'fee': fee, 'qty': numContracts}
 	return putDictionary
-
 
 
 
@@ -100,7 +100,7 @@ def strangleEth(lowRange, highRange, rangeStep):
 
 
 
-def strangleBtc(lowRange, highRange, rangeStep):
+def strangleBtcOg(lowRange, highRange, rangeStep):
 	strangleResultList = []
 	for expiryPrice in np.arange(lowRange, highRange, rangeStep):
 		testCall = callReturnFunc('btc', 50000, expiryPrice, 1, 1200)
@@ -111,21 +111,56 @@ def strangleBtc(lowRange, highRange, rangeStep):
 
 
 
+def strangleBtc(lowRange, highRange, rangeStep, strike, callCost, callQty, putCost, putQty):
+	strangleResultList = []
+	for expiryPrice in np.arange(lowRange, highRange, rangeStep):
+		testCall = callReturnFunc('btc', strike, expiryPrice, callQty, callCost)
+		testPut = putReturnFunc('btc', strike, expiryPrice, putQty, putCost)
+		strangle = strangleFunc(testCall, testPut)
+		strangleResultList.append(strangle)
+	return strangleResultList
+
+
+
+def getPlotLists(strangleList):
+	expiryList, netList, strikeList = [], [], []
+
+	for strangleResult in strangleList:
+		#print(strangleResult)
+		strangleCost = ro2(strangleResult['putCost'] + strangleResult['callCost'])
+		currentExpiry = strangleResult['expiry']
+		currentNet = strangleResult['net']
+		currentStrike = strangleResult['putStrike']
+		expiryList.append(currentExpiry)
+		netList.append(currentNet)
+		strikeList.append(currentStrike)
+
+	plotDict = {'expiry': expiryList, 'net': netList, 'strike': strikeList}
+
+	return plotDict
+
 
 
 #strangleEth = strangleEth(3000, 5000, 200)
-strangleBtc = strangleBtc(42000, 60001, 250)
+strangleBtc0 = strangleBtc(39999, 60001, 250, 50000, 1200, 1, 2900, 2)
+strangleBtc1 = strangleBtc(39999, 60001, 250, 50000, 1110, 1, 2819, 1)
 
 
 
-#for strangleResult in strangleEth:
-for strangleResult in strangleBtc:
-	strangleCost = ro2(strangleResult['putCost'] + strangleResult['callCost'])
-	print(strangleResult)
+plotBtc0 = getPlotLists(strangleBtc0)
+plotBtc1 = getPlotLists(strangleBtc1)
 
+expiryList0, netList0 = plotBtc0['expiry'], plotBtc0['net']
 
+expiryList1, netList1 = plotBtc1['expiry'], plotBtc1['net']
 
+fig = plt.figure(figsize=(8, 4))
+ax1, ax2 = fig.add_subplot(121), fig.add_subplot(122)
 
+ax1.plot(expiryList0, netList0)
+ax2.plot(expiryList1, netList1)
+
+plt.show()
 
 
 
