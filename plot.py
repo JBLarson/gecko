973ff660fc41f4ac61638eb489cd74b6df7d7de5
JB Larson <68449234+JBLarson@users.noreply.github.com>
2022-06-12 16:59:20 -0400

    Update plot.py

--- plot.py
+++ plot.py
@@ -2,14 +2,10 @@
 
 
 import sys
+import json
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
-import time
-import datetime
-import dateparser
-import json
-import math
 import matplotlib.pyplot as plt
 
 #lambda functions for rounding
@@ -20,8 +16,6 @@ ro1, ro2, ro6, ro8 = lambda x : round(x, 1), lambda x : round(x, 2), lambda x : 
 jsonInAddr = 'data/geckoAnalysis2.json'
 with open(jsonInAddr, 'r') as f:
 	geckoData = json.load(f)
-
-
 
 
 def plotTokenFunc(tokenSymbol, startDateIndex, endDateIndex):
@@ -51,17 +45,10 @@ def plotTokenFunc(tokenSymbol, startDateIndex, endDateIndex):
 		volumes.append(tokenPairVolume[key])
 
 
-	#plt.subplot(211)
-
-
-
 	plt.plot(smaDates, prices, label="Price")
 	plt.plot(smaDates, sma7Prices, label="7-Day SMA")
-
-
 	plt.plot(smaDates, sma30Prices, label="30-Day SMA")
 	plt.plot(smaDates, sma50Prices, label="50-Day SMA")
-
 	plt.plot(smaDates, sma200Prices, label="200-Day SMA")
 
 	plt.ylabel('Price')
@@ -89,72 +76,42 @@ def window():
 	# buttons for pairs
 	p1 = QPushButton(win)
 	p1.setText("Plot BtcUsd")
-	p1.move(20,20)
+	p1.move(20,40)
 	p1.clicked.connect(plotBtcUsd)
 
-	p2 = QPushButton(win)
-	p2.setText("Plot BtcEur")
-	p2.move(20,60)
-	p2.clicked.connect(plotBtcEur)
-	
 	p3 = QPushButton(win)
 	p3.setText("Plot EthUsd ")
-	p3.move(20,100)
+	p3.move(20,120)
 	p3.clicked.connect(plotEthUsd)
 	
-	p4 = QPushButton(win)
-	p4.setText("Plot EthEur")
-	p4.move(20,140)
-	p4.clicked.connect(plotEthEur)
-
 	p5 = QPushButton(win)
-	p5.setText("Plot AdaUsd")
-	p5.move(140,20)
-	p5.clicked.connect(plotAdaUsd)
-	
-	p6 = QPushButton(win)
-	p6.setText("Plot AdaEur ")
-	p6.move(140,60)
-	p6.clicked.connect(plotAdaEur)
-	
+	p5.setText("Plot KavaUsd")
+	p5.move(140,40)
+	p5.clicked.connect(plotKavaUsd)
+
 	p7 = QPushButton(win)
 	p7.setText("Plot XmrUsd")
-	p7.move(140,100)
+	p7.move(140,120)
 	p7.clicked.connect(plotXmrUsd)
 
-	p8 = QPushButton(win)
-	p8.setText("Plot XmrEur")
-	p8.move(140,140)
-	p8.clicked.connect(plotXmrEur)
-	
 	p9 = QPushButton(win)
-	p9.setText("Plot LinkUsd ")
-	p9.move(260,20)
-	p9.clicked.connect(plotLinkUsd)
-	
-	p10 = QPushButton(win)
-	p10.setText("Plot LinkEur")
-	p10.move(260,60)
-	p10.clicked.connect(plotLinkUsd)
+	p9.setText("Plot AtomUsd ")
+	p9.move(260,40)
+	p9.clicked.connect(plotAtomUsd)
 
 	p11 = QPushButton(win)
 	p11.setText("Plot DaiUsd")
-	p11.move(260,100)
+	p11.move(260,120)
 	p11.clicked.connect(plotDaiUsd)
-	
-	p12 = QPushButton(win)
-	p12.setText("Plot DaiEur ")
-	p12.move(260,140)
-	p12.clicked.connect(plotDaiEur)
 	
 	p13 = QPushButton(win)
 	p13.setText("Close Chart ")
-	p13.move(200,240)
+	p13.move(200,200)
 	p13.clicked.connect(closeFig)
 	
 	p14 = QPushButton(win)
 	p14.setText("Quit GUI ")
-	p14.move(100,240)
+	p14.move(100,200)
 	p14.clicked.connect(quitPlot)
 
 
@@ -178,43 +135,25 @@ def closeFig():
 def plotBtcUsd():
 	pltBtcUsd = plotTokenFunc('BtcUsd', sdi, edi)
 
-def plotBtcEur():
-	pltBtcEur = plotTokenFunc('BtcEur', sdi, edi)
-
 def plotEthUsd():
 	pltEthUsd = plotTokenFunc('EthUsd', sdi, edi)
-
-def plotEthEur():
-	pltEthEur = plotTokenFunc('EthEur', sdi, edi)
 
 
 def plotDaiUsd():
 	pltDaiUsd = plotTokenFunc('DaiUsd', sdi, edi)
 
-def plotDaiEur():
-	pltDaiEur = plotTokenFunc('DaiEur', sdi, edi)
 
+def plotKavaUsd():
+	pltAdaUsd = plotTokenFunc('KavaUsd', sdi, edi)
 
-def plotAdaUsd():
-	pltAdaUsd = plotTokenFunc('AdaUsd', sdi, edi)
-
-def plotAdaEur():
-	pltAdaEur = plotTokenFunc('AdaEur', sdi, edi)
-
-def plotLinkUsd():
-	pltLinkUsd = plotTokenFunc('LinkUsd', sdi, edi)
-
-def plotLinkEur():
-	pltLinkEur = plotTokenFunc('LinkEur', sdi, edi)
+def plotAtomUsd():
+	pltLinkUsd = plotTokenFunc('AtomUsd', sdi, edi)
 
 
 def plotXmrUsd():
 	pltXmrUsd = plotTokenFunc('XmrUsd', sdi, edi)
 
-def plotXmrEur():
-	pltXmrEur = plotTokenFunc('XmrEur', sdi, edi)
 
 
 if __name__ == '__main__':
 	window()
-
