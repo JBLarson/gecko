c9f4254e7210dcb7aacaddd3e622d65d1f30e6b5
JB Larson <68449234+JBLarson@users.noreply.github.com>
2022-06-12 16:58:55 -0400

    Update and rename sma.py to movingAvgs.py

---
+++ movingAvgs.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+
+from geckoFuncz import *
+import json
+import math
+import dateparser
+from datetime import datetime, timedelta, date
+from time import strftime
+import numpy as np
+#lambda functions for rounding
+ro1, ro2, ro6, ro8 = lambda x : round(x, 1), lambda x : round(x, 2), lambda x : round(x, 6), lambda x : round(x, 8)
+
+
+time = datetime.now()
+dtRn = str(strftime("%x") + " " + strftime("%X"))
+justTime, justDate = strftime("%X"), strftime("%x")
+print("\nStarted 7-day, 30-day, 50-day & 200-day SMA Script on: " + str(justDate) + " at: " + str(justTime) + "\n")
+
+
+
+# import gecko analysis data
+jsonInAddr = 'data/geckoAnalysis.json'
+
+with open(jsonInAddr, 'r') as f:
+	geckoData = json.load(f)
+
+geckoKeys = list(geckoData.keys())
+
+
+def createMovingAvgDict(tokenPair, n):
+	tokenData = geckoData[tokenPair]['data']
+
+	movingAvgDict = {}
+	for date in tokenData:
+		movingAvgN = movingAvgFunc(tokenData, date, n)
+		movingAvgDict.update(movingAvgN)
+
+	return movingAvgDict
+
+
+for geckoKey in geckoKeys:
+	currentMovingAvg7Dict = createMovingAvgDict(geckoKey, 7)
+	currentMovingAvg30Dict = createMovingAvgDict(geckoKey, 30)
+	currentMovingAvg50Dict = createMovingAvgDict(geckoKey, 50)
+	currentMovingAvg200Dict = createMovingAvgDict(geckoKey, 200)
+
+	currentGeckoData = geckoData[geckoKey]
+
+	currentGeckoData['movingAvg7'] = currentMovingAvg7Dict
+	currentGeckoData['movingAvg30'] = currentMovingAvg30Dict
+	currentGeckoData['movingAvg50'] = currentMovingAvg50Dict
+	currentGeckoData['movingAvg200'] = currentMovingAvg200Dict
+
+	print("Added 7-day, 30-day, 50-day, & 200-day SMA for: " + str(currentGeckoData['pair']))
+
+
+
+time = datetime.now()
+dtRn = str(strftime("%x") + " " + strftime("%X"))
+justTime, justDate = strftime("%X"), strftime("%x")
+print("\nCompleted sort on: " + str(justDate) + " at: " + str(justTime) + "\n")
+
+
+
+
+jsonOutAddr = 'data/geckoAnalysis2.json'
+try:
+	with open(jsonOutAddr, 'w') as fp1: json.dump(geckoData, fp1)
+
+	print("\nSuccess Creating Sorted Trade JSON file at: " + str(jsonOutAddr) + '\n')
+
+
+except Exception as e: print(e)
+
+
+
+
