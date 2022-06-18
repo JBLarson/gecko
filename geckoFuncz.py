114a8dcf6714ac05735401d639acd80bb43110e0
JB Larson <68449234+JBLarson@users.noreply.github.com>
2022-06-18 16:26:39 -0400

    issue with datetime library

--- geckoFuncz.py
+++ geckoFuncz.py
@@ -1,12 +1,42 @@
 
-from time import localtime, strftime
+from time import *
+from datetime import *
 import time
-from datetime import timedelta
-import datetime
 import json
 import dateparser
 import math
 import numpy as np
+
+
+
+
+
+def createJsonFunc(jsonOutAddr, jsonData):
+	try:
+		with open(jsonOutAddr, 'w') as fp1: json.dump(jsonData, fp1)
+		functionOutput = ("\nSuccess Creating JSON at: " + str(jsonOutAddr) + "\n")
+
+	except Exception as e:
+		functionOutput = "\nFailed to create JSON. Error msg:\n" + str(e)
+
+	return functionOutput
+
+
+
+
+def readJsonFunc(jsonInAddr):
+	with open(jsonInAddr, 'r') as r:
+		jsonOutputDict = json.load(r)
+	return jsonOutputDict
+
+
+
+
+
+
+
+
+
 
 # ---
 # for api request part of fetchGecko
